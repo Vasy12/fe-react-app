84d5a2e1e216508a9c331f1ae404a73417528659
User Name <artur.it2914@gmail.com>
2020-07-31 20:50:03 +0300

    Add Stopwatch component

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,20 @@
-import React, { Component } from 'react';
-import UsersList from './components/UsersList';
-import SelectedUsersList from './components/SelectedUsersList';
+import React, { useState } from 'react';
+import Stopwatch from './components/Stopwatch';
 
-const dbData = [
-  {
-    id: 1,
-    name: 'Test',
-    surname: 'Testovich',
-  },
-  {
-    id: 2,
-    name: 'Test2',
-    surname: 'Testovich2',
-  },
-  {
-    id: 3,
-    name: 'Test3',
-    surname: 'Testovich3',
-  },
-  {
-    id: 4,
-    name: 'Test4',
-    surname: 'Testovich4',
-  },
-];
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: dbData.map(u => ({ ...u, isSelected: false })),
-    };
-  }
-
-  setUsers = newUsers => {
-    this.setState({
-      users: newUsers,
-    });
-  };
-
-  render() {
-    const { users } = this.state;
-    return (
-      <>
-        <UsersList users={users} setUsers={this.setUsers} />
-        <SelectedUsersList users={users} />
-      </>
-    );
-  }
-}
+const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        switch
+      </button>
+      {isVisible && <Stopwatch />}
+    </>
+  );
+};
 
 export default App;
