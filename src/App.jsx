cb3d866837804bfd1da1bb971ac5a4e9ec040ca7
User Name <artur.it2914@gmail.com>
2020-07-31 18:11:14 +0300

    Add UsersList to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,12 @@
-import React, { Component } from 'react';
-import Aloha from './components/Aloha';
+import React from 'react';
+import UsersList from './components/UsersList';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      guests: [
-        {
-          id: 1,
-          firstName: 'Test',
-          lastName: 'Testovich',
-        },
-        {
-          id: 85684,
-          firstName: 'Maria',
-          lastName: 'Testovna',
-        },
-        {
-          id: 37126365,
-          firstName: 'Misha',
-          lastName: 'Ivanov',
-        },
-        {
-          id: 4,
-          firstName: 'Fred',
-          lastName: 'Fredovich',
-        },
-      ],
-    };
-
-    this.switchSortDir = false;
-  }
-
-  sortGuests = () => {
-    const { guests } = this.state;
-
-    const sortedGuests = JSON.parse(JSON.stringify(guests)).sort((a, b) => {
-      if (a.firstName > b.firstName) {
-        return this.switchSortDir ? -1 : 1;
-      }
-      if (a.firstName < b.firstName) {
-        return this.switchSortDir ? 1 : -1;
-      }
-      return 0;
-    });
-
-    this.setState({
-      guests: sortedGuests,
-    });
-    this.switchSortDir = !this.switchSortDir;
-  };
-
-  mapGuests = () =>
-    this.state.guests.map(({ id, firstName, lastName }) => (
-      <Aloha key={id} name={`${id} ${firstName} ${lastName}`} />
-    ));
-
-  render() {
-    return (
-      <>
-        <button onClick={this.sortGuests}>SORT</button>
-        <div>{this.mapGuests()}</div>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <UsersList />
+    </>
+  );
+};
 
 export default App;
