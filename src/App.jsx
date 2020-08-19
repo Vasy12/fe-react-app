999e4d25b7d6db3d1f239cc658eead25d6d08a61
User Name <artur.it2914@gmail.com>
2020-08-19 17:42:48 +0300

    Add example of censorship

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import ViewportSize from './components/ViewportSize';
-import { useState } from 'react';
+import Chat from './components/Chat';
 
 const App = () => {
-  const [isVisible, setIsVisible] = useState(true);
-
-  return (
-    <>
-      <button onClick={() => setIsVisible(!isVisible)}>
-        set {isVisible ? 'visible' : 'invisible'}
-      </button>
-      {isVisible && <ViewportSize />}
-    </>
-  );
+  return <Chat />;
 };
 
 export default App;
